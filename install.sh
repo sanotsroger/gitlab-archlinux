11605a80e8b56ed89ef20a14d4806242ca3e1775
Roger Santos <rogerioantoniodossantos@outlook.com>
2024-03-25 23:36:54 -0300

    Corrent endline mirrorlist

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ PACMAN_PARALLELDOWNLOADS=5
 pacman-key --init \
 && pacman-key --populate archlinux \
 && sed 's/ParallelDownloads = \d+/ParallelDownloads = ${PACMAN_PARALLELDOWNLOADS}/g' -i /etc/pacman.conf \
-&& sed 's/NoProgressBar/#NoProgressBar/g' -i /etc/pacman.conf
+&& sed 's/NoProgressBar/#NoProgressBar/g' -i /etc/pacman.conf \
 && sed -i 's/^Server = https:\/\/.*/Server = https:\/\/archlinux.c3sl.ufpr.br\/$repo\/os\/$arch/' /etc/pacman.d/mirrorlist
 
 # Update system
