50e1c5071f86c79d830dfc5654d572679d7334e2
Roger Santos <rogerioantoniodossantos@outlook.com>
2024-03-25 22:54:41 -0300

    Add

---
+++ install.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+
+PACMAN_PARALLELDOWNLOADS=5
+
+pacman-key --init \
+&& pacman-key --populate archlinux \
+&& sed 's/ParallelDownloads = \d+/ParallelDownloads = ${PACMAN_PARALLELDOWNLOADS}/g' -i /etc/pacman.conf \
+&& sed 's/NoProgressBar/#NoProgressBar/g' -i /etc/pacman.conf
+&& sed -i 's/^Server = https:\/\/.*/Server = https:\/\/archlinux.c3sl.ufpr.br\/$repo\/os\/$arch/' /etc/pacman.d/mirrorlist
+
+# Update system
+pacman -Syyu --noconfirm \
+    curl \
+    wget \
+    openssh \
+    ; pacman -Rns $(pacman -Qtdq) \
+    ; pacman -Scc --noconfirm \
+    ; rm -Rf /var/cache/pacman/pkg/*
